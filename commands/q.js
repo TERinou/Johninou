9c3be02297141d79c16735fb52b0c5bd464f5ed5
Someone Someone <Anooxi72@gmail.com>
2021-02-11 11:15:02 +0100

    ajout d'une fonction help et fix de q

--- commands/q.js
+++ commands/q.js
@@ -11,6 +11,17 @@ module.exports = {
         message.reply("Je suis entrain de vous trouver une question");
         const reply = await calculLong();
         message.reply("Ma question est : " + reply);
+        const filter = m => m.author.id === message.author.id;
+        await message.channel.awaitMessages(filter,{
+            max: 1,
+            time: 30000,
+            errors:['time']
+        }).then(message => {
+            message = message.first();
+            message.reply("Vous avez répondu : " + message.content);
+        }).catch(collected => {
+            message.reply("Aucune réponse :(");
+        })
     }
 }
 
